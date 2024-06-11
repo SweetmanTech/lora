1a4ebf6d5ba3bcd3b71b5e8de05759214f5fa915
Sweets Sweetman <sweetmantech@gmail.com>
2024-06-11 08:40:26 -0500

    I remove unused packages.

--- components/LandingPage/LandingPageHeader.tsx
+++ components/LandingPage/LandingPageHeader.tsx
@@ -1,3 +1,5 @@
+import { DESCRIPTION, TITLE } from '@/lib/consts';
+
 const LandingPageHeader = () => (
   <div
     style={{ display: 'flex' }}
@@ -8,13 +10,13 @@ const LandingPageHeader = () => (
       className="text-4xl font-bold tracking-tighter sm:text-5xl"
       tw="text-7xl font-black pt-[100px]"
     >
-      party zora collect.
+      {TITLE}.
     </p>
     <p
       className="max-w-[600px] text-md md:text-xl font-bold"
       tw="max-w-[600px] text-4xl font-bold mt-[-25]"
     >
-      bring your own party. collect on zora.
+      {DESCRIPTION}.
     </p>
   </div>
 );
